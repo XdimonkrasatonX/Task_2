04e68f30a192c08b014d995e3432d177c08e2cad
Dima <60436788+XdimonkrasatonX@users.noreply.github.com>
2025-07-15 04:22:37 +0200

    index.php

--- exceltask/index.php
+++ exceltask/index.php
@@ -5,7 +5,11 @@
     <title><?php echo $title; ?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
-<body>
-<header>
 
-</header>
+<div class="container mt-2">
+
+    <form action="index.php" method="post">
+        <input type="file" name="file" placeholder="Добавьте файл" class="form-control mb-2">
+        <input type="submit" value="Отправить" class="btn btn-success">
+    </form>
+</div>
