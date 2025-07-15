cfdfefd92621a4e0e875a8c5cc39eef123cb5cc2
Dima <60436788+XdimonkrasatonX@users.noreply.github.com>
2025-07-15 04:12:54 +0200

    Add files via upload

---
+++ exceltask/index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="ru">
+<head>
+<meta charset="UTF-8">
+    <title><?php echo $title; ?></title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+</head>
+<body>
+<header>
+
+</header>
